bfaa692ceec030c7e5e7c358088bb456dfef82a9
romanharv1 <romanharv1@csu.fullerton.edu>
2019-09-21 14:32:24 -0700

    Created a program that prints a rectangle with random length.

--- prob03/main.cpp
+++ prob03/main.cpp
@@ -1,1 +1,32 @@
 // Random Rectangle
+
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
+#include <string>
+
+int main()
+{
+
+// Defining maximum value for random variable.
+  const int MAX_LENGTH = 40;
+
+// Creating pool for random variables.
+  unsigned seed = time(0);
+  srand(seed);
+
+  // Defining random variable.
+  int num;
+  num = rand() % MAX_LENGTH + 1;
+
+  // Assigning the random variable to an amount of #'s.
+  std::string eq;
+  eq.assign(num, '#');
+
+  // Printing rectangle.
+  std::cout << eq << std::endl;
+  std::cout << eq << std::endl;
+  std::cout << eq << std::endl;
+
+  return 0;
+}
